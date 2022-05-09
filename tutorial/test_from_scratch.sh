62a4eb0a43f16932ceb72d8a2cbf1f7558e3f326
tguillemLSST <tguillem@cca010.in2p3.fr>
2022-05-09 10:50:48 +0200

    up

--- tutorial/test_from_scratch.sh
+++ tutorial/test_from_scratch.sh
@@ -4,4 +4,4 @@ source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/w_2022_01/loadLSST.zsh
 setup lsst_distrib
 git clone https://github.com/tguillemLSST/focal_plane_analysis.git
 cd focal_plane_analysis/tutorial
-python play_with_images.py
+python images_butler.py
