89cb5622e98e1924524646375295f14a7756df8f
tguillemLSST <tguillem@sdfiana013.sdf.slac.stanford.edu>
2026-03-26 03:25:32 -0700

    up

---
+++ coadds/access_dp2_coadds.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+##########
+# Author: T. Guillemin
+# Goal: access DP2 coadds
+##########
+
+import lsst.daf.butler as dafButler
+import lsst.geom as geom
+
+#butler
+butler=dafButler.Butler('dp2_prep',collections='LSSTCam/runs/DRP/DP2/v30_0_0/DM-53881/stage3',skymap='lsst_cells_v2')
+registry = butler.registry
+
+#get trach and patch from on-sky geometrical info
+ra, dec = 53.8, -28.1
+radec = geom.SpherePoint(ra, dec, geom.degrees)
+skymap = butler.get("skyMap")
+tractInfo = skymap.findTract(radec)
+tract = tractInfo.getId()
+patchInfo = tractInfo.findPatch(radec)
+patch = tractInfo.getSequentialPatchIndex(patchInfo)
+
+bands = ['u','g','r','i','z','y']
+for i_band in range(len(bands)):
+        coaddId = {'tract': tract, 'patch': patch, 'band': bands[i_band]}
+        coadd = butler.get('deep_coadd', dataId=coaddId)
+        print(bands[i_band] + ' : ' + str(len(coadd.getInfo().getCoaddInputs().visits['visit'])))
+        image = coadd.getImage().getArray()
+        print(image)
